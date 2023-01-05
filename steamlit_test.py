dabc2c98748b5facd3089d9922be3a05f53a948c
alx199103 <nakagawa@tccwave.co.jp>
2023-01-05 20:50:07 +0900

    serd:

--- steamlit_test.py
+++ steamlit_test.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
 
 st.title('Streamlit 超入門')
 
 st.write('DataFrames')
-st.write('doudesuka')
+
+df = pd.DataFrame({
+    '1列名':[1,2,3,4],
+    '2列名':[10,20,30,40],
+})
